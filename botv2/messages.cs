4c8a91587ccbf68bda4eb734662d98a0f4cca192
Vidak Kavedzic <vidakk@gmail.com>
2024-10-17 23:32:09 +0200

    Update messages.cs

--- botv2/messages.cs
+++ botv2/messages.cs
@@ -7,8 +7,9 @@ public static class Messages
         "Navigating the current political landscape is hard, navigating the current z0r chain is not, {0}!",
         "Then God looked over all he had made, and he saw that {0} had fucked it up",
         "Once upon a time there was a z0r chain. \"Was\" because you fucked it up {0}",
-        "And I looked, and behold a pale horse: and his name that sat on him was Death, and Failure followed with him. Your failure, {0}",
+        "And I looked, and behold, a pale horse! And his name that sat on him was Death, and Failure followed with him. Your failure, {0}",
         "Enemies are red, allies are blue, {0} can't z0r, so it's a timeout for you",
-        "Do you want a doomsday today, {0}?"
+        "Do you want a doomsday today, {0}?",
+        "Way to fuck up the chain {0}"
     };
 }
